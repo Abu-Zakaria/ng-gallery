fe553517d7bdf6bcba13b9aa8bd1f49539ce50ef
Jakaria Blaine <jakariablaine120@gmail.com>
2017-07-03 17:28:24 +0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './web/home/home.component';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { LogoComponent } from './partials/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
