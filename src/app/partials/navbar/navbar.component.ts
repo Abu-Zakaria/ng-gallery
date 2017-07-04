c64cb2c20bcff58dd09374c008d806c5d4c64fc3
Jakaria Blaine <jakariablaine120@gmail.com>
2017-07-05 03:08:36 +0600

    Add js funtionalities to the drop-down menu

--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -6,9 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  showDropdown: boolean = false;
 
-  constructor() {
-    
+  constructor()
+  {
+    this.observeScreenClick()
+  }
+
+  observeScreenClick()
+  {
+    document.addEventListener('click', (e) =>
+      {
+        let avatarEl = document.querySelector('.avatar-image')
+        if (e.target != avatarEl)
+        {
+          this.showDropdown = false;
+        }
+
+      })
   }
 
   ngOnInit() {
